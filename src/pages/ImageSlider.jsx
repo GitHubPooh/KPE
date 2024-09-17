69e037caa2e0d25394badb6c1dd4b8419026fe48
Mahesh Loya <mahesh.loya30@gmail.com>
2024-09-17 19:17:49 +0530

    added slider

--- src/pages/ImageSlider.jsx
+++ src/pages/ImageSlider.jsx
@@ -1,72 +1,34 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { object } from 'prop-types';
-import { auto } from '@popperjs/core';
+// import '../../scss/_ImageSlider.css'; // Import your CSS file
 
 const ImageSlider = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        width: 100,
-        height: 120,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000, // Time in milliseconds
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-      };
-      
+  const [images, setImages] = useState([
+    "https://kpenggs.com/business_guru_admin/Product_Image/623703966.jpg",
+    "https://kpenggs.com/business_guru_admin/Product_Image/873375907.jpeg",
+    "https://kpenggs.com/business_guru_admin/Product_Image/873431237.jfif",
+    "https://kpenggs.com/business_guru_admin/Product_Image/873497315.jfif"
+  ]);
+
+  useEffect(() => {
+    // Fetch images from an API or other source if needed
+    // Replace this with your actual image fetching logic
+    // setImages(fetchedImages);
+  }, []);
 
   return (
-    <div className="slider-container" width= "100%" margin= "0 auto" >
-      <Slider {...settings}>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/623703966.jpg" alt="Slide 1" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873375907.jpeg" alt="Slide 2" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873431237.jfif" alt="Slide 3" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873497315.jfif" alt="Slide 4" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873559655.jfif" alt="Slide 5" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873578876.jpeg" alt="Slide 6" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873600175.jpeg" alt="Slide 7" className="slider-image" />
-        </div>
-        <div>
-          <img src="https://kpenggs.com/business_guru_admin/Product_Image/873642040.jpg" alt="Slide 8" className="slider-image" />
-        </div>
-      </Slider>
-    </div>
-  );
-};
-
-const NextArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <div className="slick-arrow slick-next" onClick={onClick}>
-      <i className="fas fa-chevron-right"></i>
-    </div>
-  );
-};
-
-const PrevArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <div className="slick-arrow slick-prev" onClick={onClick}>
-      <i className="fas fa-chevron-left"></i>
-    </div>
+    <Carousel className="custom-carousel">
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100"
+            src={image}
+            alt={`Slide ${index + 1}`}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
