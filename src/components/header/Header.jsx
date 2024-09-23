d72cd2bfc70266863f37119cd88043d6e2bf8693
Mahesh Loya <mahesh.loya30@gmail.com>
2024-09-23 20:19:25 +0530

    changes navbar toggler alignment

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,44 +2,46 @@ import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NoticeBar = () => {
-return (
-<div  className="container-fluid p-0 bg-light d-none d-lg-block">
-   <div  className="row gx-0 d-lg-flex">
-      <div  className="col-lg-6 col-sm-12 col-md-4 px-0 text-start">
-         <div  className="h-100 d-inline-flex align-items-center py-0 me-0">
-            <a  href="/index.html" className="navbar-brand d-flex sticky-top px-4 px-lg-2"> <img src="https://kpenggs.com/assets/images/logoimg.jfif" 
-                     alt="Logo" style={{ height: '80px', width: '115px' }} /> </a> 
+  return (
+    <div className="container-fluid p-0 bg-light">
+      <div className="row gx-0">
+        <div className="col-lg-6 col-sm-12 col-md-4 px-0 text-start">
+          <div className="h-100 d-inline-flex align-items-center py-0 me-0">
+            <a href="/index.html" className="navbar-brand d-flex sticky-top px-4 px-lg-2">
+              <img src="https://kpenggs.com/assets/images/logoimg.jfif" 
+                   alt="Logo" style={{ height: '80px', width: '115px' }} />
+            </a>
             <div>
-               <div >
-                  <h5  className="top_heading1 mt-4" style={{ margin :"10px" }}>KP Engineering Services</h5>
-                  <h6  className="top_heading_design1" style={{ margin :"10px" }}>Knowledgeable &amp; Professional </h6>
-               </div>
+              <div>
+                <h5 className="top_heading1 mt-4" style={{ margin: "10px" }}>KP Engineering Services</h5>
+                <h6 className="top_heading_design1" style={{ margin: "10px" }}>Knowledgeable &amp; Professional</h6>
+              </div>
             </div>
-         </div>
-      </div>
-      <div className="col-lg-6 d-none d-lg-flex justify-content-end">
-         <div className="h-100 d-inline-flex align-items-center py-0 me-3">
-            <small className="fa fa-phone-alt" style={{ color: "#f88f4a" }}></small> {/* Phone icon */}
+          </div>
+        </div>
+        <div className="col-lg-6 d-flex justify-content-end">
+          <div className="h-100 d-inline-flex align-items-center py-0 me-3">
+            <small className="fa fa-phone-alt" style={{ color: "#f88f4a" }}></small>
             <small style={{ color: "black", fontSize: "14px" }}>+91 8856032610</small>
-         </div>
-         <div className="h-100 d-inline-flex align-items-center">
+          </div>
+          <div className="h-100 d-inline-flex align-items-center">
             <a href="#" className="btn btn-sm-square bg-white me-1">
-            <i className="fab fa-facebook-f" style={{ color: "#f88f4a" }}></i> {/* Facebook icon */}
+              <i className="fab fa-facebook-f" style={{ color: "#f88f4a" }}></i>
             </a>
             <a href="#" className="btn btn-sm-square bg-white me-1">
-            <i className="fab fa-twitter" style={{ color: "#f88f4a" }}></i> {/* Twitter icon */}
+              <i className="fab fa-twitter" style={{ color: "#f88f4a" }}></i>
             </a>
             <a href="#" className="btn btn-sm-square bg-white me-1">
-            <i className="fab fa-youtube" style={{ color: "#f88f4a" }}></i> {/* YouTube icon */}
+              <i className="fab fa-youtube" style={{ color: "#f88f4a" }}></i>
             </a>
             <a href="#" className="btn btn-sm-square bg-white me-0">
-            <i className="fab fa-instagram" style={{ color: "#f88f4a" }}></i> {/* Instagram icon */}
+              <i className="fab fa-instagram" style={{ color: "#f88f4a" }}></i>
             </a>
-         </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
-);
+    </div>
+  );
 };
 const NavbarComponent = ({ isFixed }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -55,7 +57,7 @@ const NavbarComponent = ({ isFixed }) => {
     >
       {/* Mobile menu toggle button */}
       <button 
-        className="navbar-toggler ms-auto  justify-content-end" 
+        className="navbar-toggler me-auto  justify-content-end" 
         type="button" 
         onClick={toggleMenu} 
         aria-controls="navbarSupportedContent" 
@@ -72,7 +74,7 @@ const NavbarComponent = ({ isFixed }) => {
         >
         
         {/* Aligning menu items to the right */}
-        <ul className="navbar-nav ms-auto justify-content-end" style={{ backgroundColor: '#f88f4a !important' }}>
+        <ul className="navbar-nav me-auto justify-content-end" style={{ backgroundColor: '#f88f4a !important' }}>
           <li className="nav-item">
             <a className="nav-link" href="#" style={{ color: 'white' }}>HOME</a>
           </li>
