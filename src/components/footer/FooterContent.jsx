d4981f6e832ad0abe92bba46db551dfd219e4515
Pooja <pkatyarmal@gmail.com>
2024-10-01 13:59:44 +0530

    hover in footer, created seperate scss file for each component on landing page

--- src/components/footer/FooterContent.jsx
+++ src/components/footer/FooterContent.jsx
@@ -19,11 +19,9 @@ const Footer = () => {
   ];
 
   return (
-    <div className="row text-light" >
-
-      {/* Added address-section class here */}
-      <div className="col-lg-6 col-md-6 col-sm-12 address-section" >
-        <h4 className="text-light" >Address</h4>
+    <div className="row text-light">
+      <div className="col-lg-6 col-md-6 col-sm-12 address-section">
+        <h4 className="text-light">Address</h4>
         <p>
           <i className="fa fa-map-marker-alt me-2"></i>Office No, 3 &amp; 4 Nanded City
         </p>
@@ -32,7 +30,7 @@ const Footer = () => {
         <p><i className="fa fa-phone-alt me-2"></i>+91 8856032610</p>
         <p><i className="fa fa-envelope me-2"></i>pemb@kpenggs.com</p>
         <p><i className="fa fa-envelope me-2"></i>info@kpenggs.com</p>
-        <div className="d-flex pt-2 mb-3" >
+        <div className="d-flex pt-2 mb-3">
           <a href="#" className="btn btn-outline-light btn-social">
             <i className="fab fa-twitter"></i>
           </a>
@@ -54,7 +52,7 @@ const Footer = () => {
           <ul className="footer-nav-link style-none text-light">
             {link.links.map((linkItem, index) => (
               <li key={index}>
-                <Link to={linkItem.href} style={{ textDecoration: "none", color: "#fff" }}>
+                <Link to={linkItem.href} className="text-link">
                   &gt; {linkItem.label}
                 </Link>
               </li>
@@ -65,7 +63,7 @@ const Footer = () => {
           <ul className="footer-nav-link style-none text-light">
             {link.links1.map((linkItem, index) => (
               <li key={index}>
-                <Link to={linkItem.href} style={{ textDecoration: "none", color: "#fff" }}>
+                <Link to={linkItem.href} className="text-link">
                   &gt; {linkItem.label}
                 </Link>
               </li>
@@ -89,7 +87,7 @@ const Footer = () => {
         <p>
           KP Engineering Services was founded in 2020 by a team of professionals with over 15 years
           of experience in American Engineering and detailing. We deliver flawless Detailing output with
-          precision and quick turn around schedules that meet our clients design and detailing requirements.
+          precision and quick turnaround schedules that meet our clients' design and detailing requirements.
         </p>
       </div>
     </div>
