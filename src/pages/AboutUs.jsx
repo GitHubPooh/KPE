541f3fc1191d88954cef70e3eb601e24089efc56
Pooja <pkatyarmal@gmail.com>
2024-10-02 15:30:04 +0530

    till about-us page pantagaon

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -10,6 +10,22 @@ const AboutUs = () => {
       <>
         <Seo pageTitle="KPE Engineering" />
         <Header />
+
+        <div class="full-width-image">
+          <img src="https://kpenggs.com/assets/images/aboutus.jfif" alt="Full Width Image"/>
+          <div class="text-overlay">ABOUT US</div>
+        </div>
+        <p class="image-description"> At KP Engineering Services, we specialize in providing accurate and timely metal 
+        building detailing services tailored to the needs of our clients in India. Our experienced team is dedicated to 
+        delivering precision in every project we undertake, including PEMB designing and detailing, hybrid structures, 
+        and cold-formed structures With a commitment to excellence and a focus on customer satisfaction, we have 
+        established ourselves as a trusted partner for clients seeking high-quality detailing services. Our expertise 
+        in handling complex projects sets us apart, allowing us to meet the diverse needs of our clients with efficiency
+        and professionalism.</p>
+
+        
+
+
   
         <div className="fancy-feature-twelve position-relative mt-300 xl-mt-200 md-mt-120" id="s2">
           <div className="container">
